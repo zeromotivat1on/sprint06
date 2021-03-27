0f89973901e88355ec00bb13140f238a628bf2c9
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-03-27 13:40:06 +0200

    t02

---
+++ t02/Team.php
@@ -0,0 +1,97 @@
+<?php
+
+    class Avenger {
+
+        public $name;
+        public $alias;
+        public $gender;
+        public $age;
+        public $power = array();
+        public $hp;
+
+        public function __construct($name, $alias, $gender, $age, $power, $hp) {
+
+            $this->name = $name;
+            $this->alias = $alias;
+            $this->gender = $gender;
+            $this->age = $age;
+            $this->power = $power;
+            $this->hp = $hp;
+
+        }
+
+    }
+
+    class Team {
+
+        private $id;
+        private $avengers = array();
+
+        public function __construct($id, $avengers) {
+
+            $this->id = $id;
+            $this->avengers = $avengers;
+
+        }
+
+        public function battle($damage) {
+
+            for($i = 0; $i < count($this->avengers); $i++) {
+
+                $this->avengers[$i]->hp -= $damage;
+
+            }
+
+        }
+
+        public function calculate_losses($cloned_team) {
+
+            $temp = 0;
+            
+            for($i = 0; $i < count($this->avengers); $i++) {
+
+                if($this->avengers[$i]->hp <= 0) {
+
+                    $temp++;
+
+                }
+
+            }
+
+            if($temp == 0) {
+
+                echo "We haven't lost anyone in this battle!\n";
+
+            } else {
+
+                echo "In this battle we lost $temp Avenger(s).\n";
+
+            }
+
+        }
+
+    }
+
+    /*
+    $arr = array();
+    
+    $arr[0] = new Avenger("Tony Stark", "Iron Man", "man", 38,["intelligence", "durability", "magnetism"], 120);
+    $arr[1] = new Avenger("Natasha Romanoff", "Black Widow", "woman", 35,["agility", "martial arts"], 75);
+    $arr[2] = new Avenger("Carol Danvers", "Captain Marvel", "woman", 27,["durability", "flight", "interstellar travel"], 95);
+    
+    $team = new Team(1, $arr);
+    
+    echo"Battle 1\n";
+    $cloned_team = clone $team;
+    $damage = 25;
+    $team->battle($damage);
+    $team->calculate_losses($cloned_team);
+    
+    echo"\nBattle 2\n";
+    $cloned_team = clone $team;
+    $damage = 80;
+    $team->battle($damage);
+    $team->calculate_losses($cloned_team);
+    */
+
+?>
