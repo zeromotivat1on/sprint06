c4f785069bbe70a1ba799342bd5c63aa3435d2ee
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-03-28 11:10:03 +0300

    t06

---
+++ t06/Normal/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    function calculate_time() {
+
+        $given = new DateTime("1939-01-01");
+        $now = new DateTime("now");
+
+        $res = $given->diff($now);
+
+        return $res;
+
+    }
+
+    $time = calculate_time();
+    echo "In real life you were absent for ". $time->format("%Y") . " years, ".$time->format("%m") . " months, ". $time->format("%d") . " days\n";
+
+    ?>
+</body>
+</html>
